b784b16fc277737ccd3415dd69fb92c2c557ec22
zyc <zhangyc@missfresh.cn>
2019-11-18 20:05:24 +0800

    fix

--- mall/src/main/java/com/copyright/mall/bean/ItemOrder.java
+++ mall/src/main/java/com/copyright/mall/bean/ItemOrder.java
@@ -1,155 +1,175 @@
 package com.copyright.mall.bean;
+
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 
 /**
- *
  * 子订单
+ *
  * @author lijian
  * @date 2019-10-10 16:30
  **/
 public class ItemOrder implements Serializable {
 
-	private static final long serialVersionUID = 5331428971875366225L;
+    private static final long serialVersionUID = 5331428971875366225L;
 
-	/**主键**/
-	private Long id;
+    /**
+     * 主键
+     **/
+    private Long id;
 
-	/**创建时间**/
-	private Date gmtCreate;
+    /**
+     * 创建时间
+     **/
+    private Date gmtCreate;
 
-	/**修改时间**/
-	private Date gmtModified;
+    /**
+     * 修改时间
+     **/
+    private Date gmtModified;
 
-	/**子订单号**/
-	private String itemOrderId;
+    /**
+     * 子订单号
+     **/
+    private String itemOrderId;
 
-	/**商铺订单号**/
-	private String shopOrderId;
+    /**
+     * 商铺订单号
+     **/
+    private String shopOrderId;
 
-	/**商品id**/
-	private Long itemId;
+    /**
+     * 商品id
+     **/
+    private Long itemId;
 
-	/**sku号**/
-	private Long skuId;
+    /**
+     * sku号
+     **/
+    private Long skuId;
 
-	/**商品订单状态**/
-	private Integer itemOrderStatus;
+    /**
+     * 商品订单状态
+     **/
+    private Integer itemOrderStatus;
 
-	/**商品价格**/
-	private Integer itemPrice;
+    /**
+     * 商品价格
+     **/
+    private Integer itemPrice;
 
-	/**商品数量**/
-	private Integer itemCount;
+    /**
+     * 商品数量
+     **/
+    private Integer itemCount;
 
-	/**商品总价**/
-	private Integer itemTotalPrice;
+    /**
+     * 商品总价
+     **/
+    private Integer itemTotalPrice;
 
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id){
-		this.id = id;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public Long getId(){
-		return this.id;
-	}
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
 
-	public void setGmtCreate(Date gmtCreate){
-		this.gmtCreate = gmtCreate;
-	}
+    public Date getGmtCreate() {
+        return this.gmtCreate;
+    }
 
-	public Date getGmtCreate(){
-		return this.gmtCreate;
-	}
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
 
-	public void setGmtModified(Date gmtModified){
-		this.gmtModified = gmtModified;
-	}
+    public Date getGmtModified() {
+        return this.gmtModified;
+    }
 
-	public Date getGmtModified(){
-		return this.gmtModified;
-	}
+    public void setItemOrderId(String itemOrderId) {
+        this.itemOrderId = itemOrderId;
+    }
 
-	public void setItemOrderId(String itemOrderId){
-		this.itemOrderId = itemOrderId;
-	}
+    public String getItemOrderId() {
+        return this.itemOrderId;
+    }
 
-	public String getItemOrderId(){
-		return this.itemOrderId;
-	}
+    public void setShopOrderId(String shopOrderId) {
+        this.shopOrderId = shopOrderId;
+    }
 
-	public void setShopOrderId(String shopOrderId){
-		this.shopOrderId = shopOrderId;
-	}
+    public String getShopOrderId() {
+        return this.shopOrderId;
+    }
 
-	public String getShopOrderId(){
-		return this.shopOrderId;
-	}
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
 
-	public void setItemId(Long itemId){
-		this.itemId = itemId;
-	}
+    public Long getItemId() {
+        return this.itemId;
+    }
 
-	public Long getItemId(){
-		return this.itemId;
-	}
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
 
-	public void setSkuId(Long skuId){
-		this.skuId = skuId;
-	}
+    public Long getSkuId() {
+        return this.skuId;
+    }
 
-	public Long getSkuId(){
-		return this.skuId;
-	}
+    public void setItemOrderStatus(Integer itemOrderStatus) {
+        this.itemOrderStatus = itemOrderStatus;
+    }
 
-	public void setItemOrderStatus(Integer itemOrderStatus){
-		this.itemOrderStatus = itemOrderStatus;
-	}
+    public Integer getItemOrderStatus() {
+        return this.itemOrderStatus;
+    }
 
-	public Integer getItemOrderStatus(){
-		return this.itemOrderStatus;
-	}
+    public void setItemPrice(Integer itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 
-	public void setItemPrice(Integer itemPrice){
-		this.itemPrice = itemPrice;
-	}
+    public Integer getItemPrice() {
+        return this.itemPrice;
+    }
 
-	public Integer getItemPrice(){
-		return this.itemPrice;
-	}
+    public void setItemCount(Integer itemCount) {
+        this.itemCount = itemCount;
+    }
 
-	public void setItemCount(Integer itemCount){
-		this.itemCount = itemCount;
-	}
+    public Integer getItemCount() {
+        return this.itemCount;
+    }
 
-	public Integer getItemCount(){
-		return this.itemCount;
-	}
+    public Integer getItemTotalPrice() {
+        return itemTotalPrice;
+    }
 
-  public Integer getItemTotalPrice() {
-    return itemTotalPrice;
-  }
+    public void setItemTotalPrice(Integer itemTotalPrice) {
+        this.itemTotalPrice = itemTotalPrice;
+    }
 
-  public void setItemTotalPrice(Integer itemTotalPrice) {
-    this.itemTotalPrice = itemTotalPrice;
-  }
-
-  @Override
-	public String toString() {
-		return "ItemOrder [ id= "+id+
-			",gmtCreate= "+gmtCreate+
-			",gmtModified= "+gmtModified+
-			",itemOrderId= "+itemOrderId+
-			",shopOrderId= "+shopOrderId+
-			",itemId= "+itemId+
-			",skuId= "+skuId+
-			",itemOrderStatus= "+itemOrderStatus+
-			",itemPrice= "+itemPrice+
-			",itemCount= "+itemCount+
-			",item TotalPrice= "+itemTotalPrice+"]";
-	}
+    @Override
+    public String toString() {
+        return "ItemOrder [ id= " + id +
+                ",gmtCreate= " + gmtCreate +
+                ",gmtModified= " + gmtModified +
+                ",itemOrderId= " + itemOrderId +
+                ",shopOrderId= " + shopOrderId +
+                ",itemId= " + itemId +
+                ",skuId= " + skuId +
+                ",itemOrderStatus= " + itemOrderStatus +
+                ",itemPrice= " + itemPrice +
+                ",itemCount= " + itemCount +
+                ",item TotalPrice= " + itemTotalPrice + "]";
+    }
 }
