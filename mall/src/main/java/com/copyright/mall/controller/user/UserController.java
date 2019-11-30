ec1eb58f6d2d75e24ec4586cad21cdfe096eaec8
zyc <zhangyc@missfresh.cn>
2019-11-30 10:56:53 +0800

    设置详情

--- mall/src/main/java/com/copyright/mall/controller/user/UserController.java
+++ mall/src/main/java/com/copyright/mall/controller/user/UserController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -48,6 +47,7 @@ public class UserController extends BaseController {
         userAddress.setCity(createAddressParam.getCity());
         userAddress.setArea(createAddressParam.getArea());
         userAddress.setStreet(createAddressParam.getStreet());
+        userAddress.setDetail(createAddressParam.getDetail());
         userAddress.setConsigneeName(createAddressParam.getConsigneeName());
         userAddress.setConsigneePhone(createAddressParam.getConsigneePhone());
         userAddressService.insertSelective(userAddress);
@@ -83,6 +83,7 @@ public class UserController extends BaseController {
         userAddress.setCity(createAddressParam.getCity());
         userAddress.setArea(createAddressParam.getArea());
         userAddress.setStreet(createAddressParam.getStreet());
+        userAddress.setDetail(createAddressParam.getDetail());
         userAddress.setConsigneeName(createAddressParam.getConsigneeName());
         userAddress.setConsigneePhone(createAddressParam.getConsigneePhone());
         userAddressService.updateByPrimaryKeySelective(userAddress);
