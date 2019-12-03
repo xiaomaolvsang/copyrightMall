04ca3cff5d1688b0282fe9e31eceeda23163c1b9
lijian <lijian@duxiaoman.com>
2019-12-03 17:27:15 +0800

    李坚

--- mall/src/main/java/com/copyright/mall/domain/requeest/product/AreaParam.java
+++ mall/src/main/java/com/copyright/mall/domain/requeest/product/AreaParam.java
@@ -16,13 +16,26 @@ import javax.validation.constraints.NotNull;
  * @version 1.0
  * @date 2019/11/24 3:44 下午
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel("productAreaParam入参")
-public class AreaParam extends BasePage {
+public class AreaParam {
+
     @ApiModelProperty("商城与版权（productArea/copyrightArea）")
     @NotBlank(message = "商城与版权不能为空")
     private String type;
+
     @ApiModelProperty("商城id")
     private Long mallId;
+
+    @ApiModelProperty(value = "当前页", required = true)
+    @NotNull(message = "页码不能为空")
+    private Integer pageNum;
+
+    /**
+     * 每页条数
+     */
+    @ApiModelProperty(value = "每页条数", required = true)
+    @NotNull(message = "页大小不能为空")
+    private int pageSize;
+
 }
