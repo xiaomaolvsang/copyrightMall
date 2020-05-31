b09aa098d3a91834e5153485390b5fcf55d2c6d0
zhangyuchen04 <zhangyuchen04@meituan.com>
2020-05-31 19:48:20 +0800

    时间类型转换

--- mall/src/main/java/com/copyright/mall/manage/domain/dto/QueryBlobListParam.java
+++ mall/src/main/java/com/copyright/mall/manage/domain/dto/QueryBlobListParam.java
@@ -1,10 +1,12 @@
 package com.copyright.mall.manage.domain.dto;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.copyright.mall.domain.dto.BasePage;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -20,9 +22,13 @@ public class QueryBlobListParam extends BasePage {
     private String blobTitle;
 
     @ApiModelProperty("起始时间")
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date starTime;
 
     @ApiModelProperty("结束时间")
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
     @ApiModelProperty("内容状态 0-正常 1-已删除")
