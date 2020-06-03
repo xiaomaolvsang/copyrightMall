07a47e79aaf1e6c2688a940fddf783209e6bb31c
zhangyuchen04 <zhangyuchen04@meituan.com>
2020-06-03 14:44:42 +0800

    空判断

--- mall/src/main/java/com/copyright/mall/manage/filter/CorsFilter.java
+++ mall/src/main/java/com/copyright/mall/manage/filter/CorsFilter.java
@@ -21,6 +21,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest)req;
         HttpServletResponse response = (HttpServletResponse)res;
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type,X-MANAGE-TOKEN");
