e4775142958e0c28d938dc6e10b11b6dcb091a04
zhangyuchen04 <zhangyuchen04@meituan.com>
2020-06-03 15:23:37 +0800

    跨域

--- mall/src/main/java/com/copyright/mall/manage/filter/ManageUserFilter.java
+++ mall/src/main/java/com/copyright/mall/manage/filter/ManageUserFilter.java
@@ -6,6 +6,7 @@ import com.copyright.mall.util.UserUtils;
 import com.copyright.mall.util.wrapper.WrapMapper;
 import io.jsonwebtoken.Claims;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import javax.annotation.Resource;
@@ -38,6 +39,11 @@ public class ManageUserFilter implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         if (((HttpServletRequest) request).getRequestURL().toString().contains("/login")) {
             chain.doFilter(request, response);
+            return;
+        }
+        // 如果是option请求，直接返回200
+        if (httpServletRequest.getMethod().equals(HttpMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
             return;
         }
         String token = httpServletRequest.getHeader("X-MANAGE-TOKEN");
