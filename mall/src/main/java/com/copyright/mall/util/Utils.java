3ce3356be559bc61f90e13cd5b2228db5480b859
zhangyuchen04 <zhangyuchen04@meituan.com>
2020-06-12 01:18:23 +0800

    切分下标

--- mall/src/main/java/com/copyright/mall/util/Utils.java
+++ mall/src/main/java/com/copyright/mall/util/Utils.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Utils {
 
     public static List<String> toStringList(String s){
-        s = s.substring(1,s.length() - 2);
+        s = s.substring(1,s.length() - 1);
         return Arrays.asList(s.split(","));
     }
 }
