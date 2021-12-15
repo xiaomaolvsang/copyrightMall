47889015be364c242b7c263ebaf4bc77e54fb17f
zyc <739781049@qq.com>
2021-12-15 18:06:08 +0800

    证书

--- mall/src/main/java/com/copyright/mall/config/TomcatConfig.java
+++ mall/src/main/java/com/copyright/mall/config/TomcatConfig.java
@@ -1,7 +1,6 @@
 package com.copyright.mall.config;
 
 import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +18,7 @@ public class TomcatConfig {
 
     private Connector createStandardConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(80);
+        connector.setPort(8080);
         return connector;
     }
 }
